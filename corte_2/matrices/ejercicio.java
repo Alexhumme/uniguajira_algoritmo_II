5c8971b266599fb5aed9ee4e6415c8ecc4f6efc7
Alexhumme <alexvaldelamarbustamante@inedimacg.edu.co>
2024-10-03 22:20:59 -0500

    ajuste, diagonales y seleccion

--- corte_2/matrices/ejercicio.java
+++ corte_2/matrices/ejercicio.java
@@ -41,17 +41,41 @@ public class ejercicio {
   public static void centerPrint(String txt) {
     int txt_w = txt.replaceAll("\\u001B\\[[;\\d]*m", "").length();
     System.out.print("|");
-    int padding = (int)((width-txt_w)/2);
+    int padding = (int) ((width - txt_w) / 2);
     for (int i = 0; i < padding; i++) {
       System.out.print(" ");
     }
     System.out.print(txt);
-    if (txt_w % 2 == width % 2) { padding--; };
+    if (txt_w % 2 == width % 2) {
+      padding--;
+    }
+    ;
     for (int i = 0; i < padding; i++) {
       System.out.print(" ");
     }
     System.out.println("|");
 
+  }
+
+  public static void sbetweenPrint(String value1, String value2) {
+    String line;
+    int spacebetween;
+
+    int txt1_w = value1.replaceAll("\\u001B\\[[;\\d]*m", "").length();
+    int txt2_w = value2.replaceAll("\\u001B\\[[;\\d]*m", "").length();
+
+    line = "";
+    line += ("\033[34m" + value1 + "\033[0m");
+
+    spacebetween = width - txt1_w - txt2_w - 3;
+
+    for (int j = 0; j < spacebetween; j++) {
+      line += " ";
+    }
+
+    line += ("\033[31m" + value2 + "\033[0m");
+
+    print(line);
   }
 
   public static void print(String txt, String label) {
@@ -69,7 +93,7 @@ public class ejercicio {
   }
 
   public static void print(int value) {
-    print(""+value);
+    print("" + value);
   }
 
   public static int input(String msg) {
@@ -78,12 +102,15 @@ public class ejercicio {
     System.out.print("\033[0m");
     return value;
   }
+
   public static int input(double msg) {
-    return input(""+msg);
+    return input("" + msg);
   }
+
   public static int input(int msg) {
-    return input(""+msg);
+    return input("" + msg);
   }
+
   public static int input() {
     return input("");
   }
@@ -94,11 +121,11 @@ public class ejercicio {
 
   public static void llenar() {
     for (int i = 0; i < matriz.length; i++) {
-     for (int j = 0; j < matriz[0].length; j++) {
-       matriz[i][j] = (int)(new Random().nextInt(10000));
-     } 
+      for (int j = 0; j < matriz[0].length; j++) {
+        matriz[i][j] = (int) (new Random().nextInt(10000));
+      }
     }
-    
+
     bottomLine();
     topLine();
     centerPrint("\033[32mELementos añadidos\033[0m");
@@ -110,12 +137,12 @@ public class ejercicio {
   public static void llenarManualmente() {
     bottomLine();
     topLine();
-    
+
     for (int i = 0; i < matriz.length; i++) {
-      print("\033[35mLlenar fila " +(i+1)+"\033[0m");
+      print("\033[35mLlenar fila " + (i + 1) + "\033[0m");
       rowLine();
       for (int j = 0; j < matriz[i].length; j++) {
-          matriz[i][j] = input((j+1)+" ");
+        matriz[i][j] = input((j + 1) + " ");
       }
       rowLine();
     }
@@ -134,25 +161,31 @@ public class ejercicio {
     int value_padding;
 
     for (int i = 0; i < matriz.length; i++) {
-      if (i != 0) rowLine();
+      if (i != 0)
+        rowLine();
       line = "";
       for (int j = 0; j < matriz[i].length; j++) {
 
-        value_padding = (int)(((width/5) - (""+matriz[i][j]).length() - 3)/2);
+        value_padding = (int) (((width / 5) - ("" + matriz[i][j]).length() - 3) / 2);
 
-        if (j > 0) line += " | ";
-        
+        if (j > 0)
+          line += " | ";
+
         for (int k = 0; k < value_padding; k++) {
-            line += " ";
+          line += " ";
         }
-        
-        line += ""+matriz[i][j];
-        
+
+        line += "" + matriz[i][j];
+
+        if ((""+matriz[i][j]).length() % 2 != 0) {
+          value_padding--;
+        }
+
         for (int k = 0; k < value_padding; k++) {
           line += " ";
         }
       }
-      print(line);
+      centerPrint(line);
     }
 
     bottomLine();
@@ -160,36 +193,41 @@ public class ejercicio {
     scanner.nextLine();
   }
 
-  public static void estadisticas () {
+  public static void estadisticas() {
     bottomLine();
     topLine();
     int suma = 0, mayor, menor;
     double promedio;
-  
+
     mayor = matriz[0][0];
     menor = matriz[0][0];
-      
+
     for (int i = 0; i < matriz.length; i++) {
       for (int j = 0; j < matriz.length; j++) {
         suma += matriz[i][j];
 
-        
-        if (matriz[i][j] > mayor) { mayor = matriz[i][j]; };
-        if (matriz[i][j] < menor) { menor = matriz[i][j]; };
-      } 
+        if (matriz[i][j] > mayor) {
+          mayor = matriz[i][j];
+        }
+        ;
+        if (matriz[i][j] < menor) {
+          menor = matriz[i][j];
+        }
+        ;
+      }
     }
-      
-    promedio = suma / (matriz.length*matriz[0].length);
 
-    print("\033[32mSuma | "+suma+"\033[0m");
+    promedio = suma / (matriz.length * matriz[0].length);
+
+    print("\033[32mSuma | " + suma + "\033[0m");
     rowLine();
-    print("\033[33mPromedio | "+promedio+"\033[0m");
+    print("\033[33mPromedio | " + promedio + "\033[0m");
     rowLine();
-    print("\033[34mMayor | "+mayor+"\033[0m");
+    print("\033[34mMayor | " + mayor + "\033[0m");
     rowLine();
-    print("\033[31mMenor | "+menor+"\033[0m");
+    print("\033[31mMenor | " + menor + "\033[0m");
     bottomLine();
-  
+
     scanner.nextLine();
     scanner.nextLine();
   }
@@ -198,29 +236,104 @@ public class ejercicio {
     bottomLine();
     topLine();
     centerPrint("\033[35mDiagonales\033[0m");
+    rowLine();
+    sbetweenPrint("\033[34mPrincipal\033[0m", "\033[31mSecundaria\033[0m");
+    rowLine();
     String line;
-    int spacebetween;
+    int value_padding;
+
     for (int i = 0; i < matriz.length; i++) {
-      line = " ";
-      line += ("\033[34m"+matriz[i][i]+"\033[0m");
+      if (i != 0)
+        rowLine();
+      line = "";
+      for (int j = 0; j < matriz[i].length; j++) {
 
-      spacebetween = 
-        width 
-        - (""+matriz[i][i]).length() 
-        - (""+matriz[i][matriz[i].length - 1 - i]).length() - 4;
+        value_padding = (int) (((width / 5) - ("" + matriz[i][j]).length() - 3) / 2);
 
-      for (int j = 0; j < spacebetween; j++) {
-        line += " "; 
+        if (j > 0)
+          line += " | ";
+
+        for (int k = 0; k < value_padding; k++) {
+          line += " ";
+        }
+
+        if (i == j && i + j + 1 == matriz[i].length) {
+          line += "\033[35m" + matriz[i][j] + "\033[0m";
+        } else {
+          if (i == j) {
+            line += "\033[34m" + matriz[i][j] + "\033[0m";
+          }
+          if (i + j + 1 == matriz[i].length) {
+            line += "\033[31m" + matriz[i][j] + "\033[0m";
+          }
+        }
+        for (int k = 0; k < value_padding; k++) {
+          line += " ";
+        }
       }
-
-      line += ("\033[31m"+matriz[i][matriz[i].length - 1 - i]+"\033[0m ");
-
-      print(line);
+      centerPrint(line);
     }
 
+    bottomLine();
     scanner.nextLine();
     scanner.nextLine();
-  } 
+  }
+
+  public static void fila_columna() {
+    bottomLine();
+    topLine();
+    
+    int row = input("Seleccine una fila (0-"+(matriz.length - 1)+")");
+    rowLine();
+    int col = input("Seleccine una columna (0-"+(matriz.length - 1)+")");
+
+    String line;
+    int value_padding;
+
+    rowLine();
+    for (int i = 0; i < matriz.length; i++) {
+      if (i != 0)
+        rowLine();
+      line = "";
+      for (int j = 0; j < matriz[i].length; j++) {
+
+        value_padding = (int) (((width / 5) - ("" + matriz[i][j]).length() - 3) / 2);
+        if (i != row && j != col) { 
+          value_padding = 4;
+        }
+
+        if (j > 0)
+          line += " | ";
+
+        for (int k = 0; k < value_padding; k++) {
+          line += " ";
+        }
+        
+        if (i == row && j == col) {
+           line += "\033[35m" + matriz[i][j] + "\033[0m";
+        } else {
+          if (i == row) {
+            line += "\033[34m" + matriz[i][j] + "\033[0m";
+          }
+          if (j == col) {
+            line += "\033[31m" + matriz[i][j] + "\033[0m";
+          }
+        }
+        if ((i == row || j == col) && (""+matriz[i][j]).length()%2 != 0) { 
+          value_padding -= 1;
+        }
+
+        for (int k = 0; k < value_padding; k++) {
+          line += " ";
+        }
+      }
+      centerPrint(line);
+    }
+
+    bottomLine();
+    scanner.nextLine();
+    scanner.nextLine();
+  }
 
   public static void reiniciar() {
     matriz = new int[5][5];
@@ -242,6 +355,7 @@ public class ejercicio {
       "Ver",
       "Estadisticas",
       "Diagonales",
+      "Seleccionar fila y columna",
       "Reiniciar",
       "Salir"
   };
@@ -251,6 +365,7 @@ public class ejercicio {
       () -> ver(),
       () -> estadisticas(),
       () -> diagonales(),
+      () -> fila_columna(),
       () -> reiniciar(),
       () -> salir()
   };
